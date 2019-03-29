151b08beb2d89d18f13985c081c45214dfbb5a1b
Minerba <proability@naver.com>
2019-03-29 18:38:00 +0900

    20190329

--- heap.cpp
+++ heap.cpp
@@ -1,55 +1,68 @@
-#ifdef DEBUG
-#include<stdio.h>
 #include<iostream>
 
 using namespace std; 
 
+struct PLAYER {
+	int pid, power, loc, cur_club; 
+	int hopelist[11]; 
+};
+
 template<class T>
 struct heap {
-	T* arr; 
 	int sz;
-	bool(*cmpFunc)(const T &a, const T &b); 
-	heap(bool(*cmptype)(const T &a, const T &b)) {
-		arr = new T[1001]; 
-		sz = 0; 
-		cmpFunc = cmptype; 
+	T *arr;
+	heap() {
+		sz = 0;
+		arr = new T[1001];
 	}
 	void push(T &a) {
-		arr[++sz] = a; 
-		upheap(); 
+		arr[++sz] = a;   // 0num  (x)  -> 1 (num)     
+		upheap();
 	}
-	void upheap() {
+
+	void upheap() {  // heapify -> up 
 		int cur = sz;
 		while (cur / 2 >= 1 && cmpFunc(arr[cur], arr[cur / 2])) {
 			SWAP(arr[cur], arr[cur / 2]);
-			cur = cur / 2; 
+			cur /= 2;
 		}
 	}
 	void pop() {
 		arr[1] = arr[sz--]; 
 		downheap(); 
 	}
+
 	void downheap() {
 		int cur = 1; 
 		while (cur * 2 <= sz) {
 			int target = cur * 2; 
-			if (target + 1 <= sz && cmpFunc(arr[target + 1], arr[target]))
+			if (target + 1 <= sz && cmpFunc(arr[target + 1], arr[target])) 
 				target++; 
 			if (cmpFunc(arr[target], arr[cur])) {
-				SWAP(arr[cur], arr[target]);
-				cur = target;
+				SWAP(arr[target], arr[cur]); 
 			}
 			else
 				break; 
+			cur *= 2; 
 		}
+
 	}
+	bool cmpFunc(PLAYER &a, PLAYER &b) {
+		if (a.power < b.power) return true; 
+		else if (a.power == b.power) {
+			if (a.pid > b.pid) return true; 
+		}
+		return false; 
+	}
+	void size() {
+		return sz; 
+	}
+
 	T& top() {
 		return arr[1]; 
 	}
-	int size() {
-		return sz; 
-	}
-	int clear() {
+
+	void clear() {
 		sz = 0; 
 	}
 	void SWAP(T &a, T &b) {
@@ -59,27 +72,17 @@ struct heap {
 	}
 };
 
-template<class T>
-bool minType(T &a, T &b) {
-	return a < b; 
-}
-
-template<class T>
-bool maxType(T &a, T &b) {
-	return a > b; 
-}
+struct CLUB {
+	int cid, loc; 
+	heap<PLAYER> priority_player; 
+}club[11];
 
 int main() {
-	heap<int> s(maxType); int x; 
-	for (int i = 0; i < 10; i++) {
-		cin >> x; 
-		s.push(x); 
+	int player_id, power; 
+	for (int i = 0; i < 3; i++) {
+		player_id = i;  cin >> power; 
+		club[0].priority_player.push({i, power});
 	}
-	int sz = s.size(); 
-	for (int i = 0; i < sz; i++) {
-		cout << s.top() << '\n'; 
-		s.pop(); 
-	}
+	cout << club[0].priority_player.size();
 	return 0; 
 }
-#endif // DEBUG
